c76602b7a8c36edbb5c6e13f393554473442ed8c
bisaksss <bisaksss@gmail.com>
2021-06-27 15:09:01 +0530

    add right bar in home page

--- social-media-frontend/src/components/rightbar/Rightbar.jsx
+++ social-media-frontend/src/components/rightbar/Rightbar.jsx
@@ -3,7 +3,61 @@ import "./rightbar.css";
 export default function Rightbar() {
     return (
         <div className="rightBar">
-            rightbar
+            <div className="rightBarcontainer">
+                <div className="birthdayContainer">
+                    <img src="/assets/cake.png" alt="" className="birthdayImage" />
+                    <span className="birthdayText">
+                       <b>Bisak Sampath</b>  and <b>3 others friends </b>have a birthday today
+                    </span>
+                </div>
+                <img src="/assets/ads2.png" alt="" className="adsImage" />
+                <h5 className="rightbarTitle">Online friends</h5>
+                <ul className="friendList">
+                    <li className="rightbarFriends">
+                        <div className="rigtbarProfileImageContainer">
+                            <img src="/assets/propic2.jpg" alt="" className="rightbarProfileImage" />
+                            <span className="rightbarOnline"></span>
+                            <span className="rightbarUserName">Bisak Sampath</span>
+                        </div>
+                    </li>
+                </ul>
+                <ul className="friendList">
+                    <li className="rightbarFriends">
+                        <div className="rigtbarProfileImageContainer">
+                            <img src="/assets/propic2.jpg" alt="" className="rightbarProfileImage" />
+                            <span className="rightbarOnline"></span>
+                            <span className="rightbarUserName">Bisak Sampath</span>
+                        </div>
+                    </li>
+                </ul>
+                <ul className="friendList">
+                    <li className="rightbarFriends">
+                        <div className="rigtbarProfileImageContainer">
+                            <img src="/assets/propic2.jpg" alt="" className="rightbarProfileImage" />
+                            <span className="rightbarOnline"></span>
+                            <span className="rightbarUserName">Bisak Sampath</span>
+                        </div>
+                    </li>
+                </ul>
+                <ul className="friendList">
+                    <li className="rightbarFriends">
+                        <div className="rigtbarProfileImageContainer">
+                            <img src="/assets/propic2.jpg" alt="" className="rightbarProfileImage" />
+                            <span className="rightbarOnline"></span>
+                            <span className="rightbarUserName">Bisak Sampath</span>
+                        </div>
+                    </li>
+                </ul>
+                <ul className="friendList">
+                    <li className="rightbarFriends">
+                        <div className="rigtbarProfileImageContainer">
+                            <img src="/assets/propic2.jpg" alt="" className="rightbarProfileImage" />
+                            <span className="rightbarOnline"></span>
+                            <span className="rightbarUserName">Bisak Sampath</span>
+                        </div>
+                    </li>
+                </ul>
+            </div>
         </div>
     )
 }
