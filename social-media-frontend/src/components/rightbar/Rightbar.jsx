84763ea4c612456c119839523e25a300932e749a
bisaksss <bisaksss@gmail.com>
2021-06-27 23:51:28 +0530

    create profile page using component

--- social-media-frontend/src/components/rightbar/Rightbar.jsx
+++ social-media-frontend/src/components/rightbar/Rightbar.jsx
@@ -2,16 +2,17 @@ import "./rightbar.css";
 import {Users} from "../../dummyData";
 import Online from "../onlinePersons/Online";
 
-export default function Rightbar() {
-    return (
-        <div className="rightBar">
-            <div className="rightBarcontainer">
-                <div className="birthdayContainer">
-                    <img src="/assets/cake.png" alt="" className="birthdayImage" />
-                    <span className="birthdayText">
-                       <b>Bisak Sampath</b>  and <b>3 others friends </b>have a birthday today
-                    </span>
-                </div>
+export default function Rightbar(profile) {
+
+    const HomeRightBar=()=>{
+        return(
+        <>
+            <div className="birthdayContainer">
+                <img src="/assets/cake.png" alt="" className="birthdayImage" />
+                <span className="birthdayText">
+                <b>Bisak Sampath</b>  and <b>3 others friends </b>have a birthday today
+                </span>
+            </div>
                 <img src="/assets/ads2.png" alt="" className="adsImage" />
                 <h5 className="rightbarTitle">Online friends</h5>
                 <ul className="friendList">
@@ -19,8 +20,75 @@ export default function Rightbar() {
                         <Online key={u.id} user={u}/>
                     ))}
                 </ul>
-   
-                
+        </>
+        )
+       
+
+    
+    };
+    const ProfileRightBar=()=>{
+        return(
+            <>
+            <h4 className="rightbarTitle">User Information</h4>
+            <div className="rightbarInfo">
+                <div className="rightbarInfoItem">
+                    <span className="rightbarInfoKey">City:</span>
+                    <span className="rightbarInfoValue">Sri Lanka</span>
+                </div>
+                <div className="rightbarInfoItem">
+                    <span className="rightbarInfoKey">From:</span>
+                    <span className="rightbarInfoValue">Badulla</span>
+                </div>
+                <div className="rightbarInfoItem">
+                    <span className="rightbarInfoKey">RelationShip:</span>
+                    <span className="rightbarInfoValue">Single</span>
+                </div>
+            </div>
+            <h4 className="rightbarTitle">User Friends</h4>
+            <div className="rightbarFollowings">
+                <div className="rightbarFollowing">
+                    <img src="/assets/propic2.jpg" alt="" className="rightbarFollowingImage" />
+                    <span className="rightbarFollowingName">Hashini Nishika</span>
+                </div>
+           
+            
+                <div className="rightbarFollowing">
+                    <img src="/assets/propic2.jpg" alt="" className="rightbarFollowingImage" />
+                    <span className="rightbarFollowingName">Hashini Nishika</span>
+                </div>
+           
+            
+                <div className="rightbarFollowing">
+                    <img src="/assets/propic2.jpg" alt="" className="rightbarFollowingImage" />
+                    <span className="rightbarFollowingName">Hashini Nishika</span>
+                </div>
+          
+          
+                <div className="rightbarFollowing">
+                    <img src="/assets/propic2.jpg" alt="" className="rightbarFollowingImage" />
+                    <span className="rightbarFollowingName">Hashini Nishika</span>
+                </div>
+         
+           
+                <div className="rightbarFollowing">
+                    <img src="/assets/propic2.jpg" alt="" className="rightbarFollowingImage" />
+                    <span className="rightbarFollowingName">Hashini Nishika</span>
+                </div>
+                <div className="rightbarFollowing">
+                    <img src="/assets/propic2.jpg" alt="" className="rightbarFollowingImage" />
+                    <span className="rightbarFollowingName">Hashini Nishika</span>
+                </div>
+            </div>
+            </>
+        )
+    };
+
+
+
+    return (
+        <div className="rightBar">
+            <div className="rightBarcontainer">
+                <ProfileRightBar/>
             </div>
         </div>
     )
---
+++ social-media-frontend/src/pages/profile/Profile.jsx
@@ -0,0 +1,40 @@
+import "./profile.css";
+import Navbar from "../../components/navBar/Navbar";
+import Leftbar from "../../components/leftbar/Leftbar";
+import Rightbar from "../../components/rightbar/Rightbar";
+import Feed from "../../components/feed/Feed";
+
+export default function Profile() {
+    return (
+        <>
+            <Navbar/> 
+        <div className="profile">
+            <Leftbar/>
+            <div className="profileRight">
+                <div className="profileRightTop">
+                    <div className="profileCover">
+                        <img src="/assets/CoverImage/cover_bisak.jpg" alt="" className="coverImage" />
+                        <img src="/assets/IMG_2198.jpg" alt="" className="profileImage" />
+                    </div>
+                    <div className="profileInfo">
+                        <h4 className="profileInfoName">Bisak Sampath</h4>
+                        
+                        <span className="profileDiscription">Hello world I'm Bisak Sampath</span>
+
+                    </div>
+                    
+                </div>
+                <div className="profileRightBottom">
+                <Feed/>
+                <Rightbar profile/>
+                </div>
+            </div>
+
+           
+         
+            
+        </div>
+    
+        </>
+    )
+}
