3456025e573fa85a025ac5ad6107320228aef322
bisaksss <bisaksss@gmail.com>
2021-06-28 13:28:35 +0530

    add routes

--- social-media-frontend/src/App.js
+++ social-media-frontend/src/App.js
@@ -2,13 +2,31 @@ import Home from "./pages/home/Home";
 import Profile from "./pages/profile/Profile";
 import Login from "./pages/login/Login";
 import Register from "./pages/register/Register"
+import {BrowserRouter as Router,Switch,Route} from "react-router-dom";
 
 function App() {
   return (
-    <Home/>
+    //<Home/>
     //<Profile/>
     //<Login/>
     //<Register/>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home/>
+        </Route>
+        <Route path="/login">
+          <Login/>
+        </Route>
+        <Route path="/register">
+          <Register/>
+        </Route>
+        <Route path="/profile/:userName">
+          <Profile/>
+        </Route>
+      </Switch>
+
+    </Router>
   );
 }
 
