8332a74ea5e984a78a55496978cabebba7bbee21
bisaksss <bisaksss@gmail.com>
2021-06-30 01:03:23 +0530

    add ref for loging form

--- social-media-frontend/src/pages/login/Login.jsx
+++ social-media-frontend/src/pages/login/Login.jsx
@@ -1,6 +1,14 @@
+import { useRef } from "react";
 import "./login.css";
 
 export default function Login() {
+   const email = useRef();
+   const password = useRef();
+
+    const handleClick=(e)=>{
+        e.preventDefault();//not refreash page
+        console.log(email.current.value);
+    }
     return (
         <div className="login">
             <div className="loginWrapper">
@@ -9,13 +17,13 @@ export default function Login() {
                     <span className="loginDiscription">Welcome to BsB Social.Find Your Frends & Post your IDEA!</span>
                 </div>
                 <div className="logingRight">
-                    <div className="loginBox">
-                        <input type="email" className="loginInput" placeholder="Email" />
-                        <input type="password" className="loginInput" placeholder="Password"  />
+                    <form className="loginBox" onSubmit={handleClick}>
+                        <input type="email" className="loginInput" placeholder="Email" ref={email}/>
+                        <input type="password" className="loginInput" placeholder="Password" ref={password} required minLength="6"/>
                         <button className="loginButoon">Log In</button>
                         <span className="loginForgot">Forgot Password</span>
                         <button className="loginregister">Create account</button>
-                    </div>
+                    </form>
                 </div>
             </div>
             
