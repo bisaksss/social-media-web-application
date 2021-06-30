aead92fc6a5747242c15c47129a42ce4defa9461
bisaksss <bisaksss@gmail.com>
2021-06-30 18:39:07 +0530

    Add recat hook and axios for register page

--- social-media-frontend/src/pages/register/Register.jsx
+++ social-media-frontend/src/pages/register/Register.jsx
@@ -1,6 +1,38 @@
+import axios from "axios";
+import { useRef } from "react";
+import { useHistory } from "react-router-dom";
 import "./register.css";
 
 export default function Register() {
+    const username=useRef();
+    const email=useRef();
+    const password=useRef();
+    const passwordAgain=useRef();
+    
+    const history=useHistory();
+
+    const saveUser=async (e)=>{
+        e.preventDefault();
+        if(passwordAgain.current.value !== password.current.value){
+            password.current.setCustomValidity("Password not match!!!!");
+        }else{
+            const user= {
+                userName:username.current.value,
+                email:email.current.value,
+                password:password.current.value,
+
+            }
+            try {
+                const res=await axios.post("auth/register",user);
+                history.push("/login");
+            } catch (error) {
+                console.log(error);
+            }
+           
+        }    
+        
+    }
+
     return (
         <div className="login">
             <div className="loginWrapper">
@@ -9,14 +41,14 @@ export default function Register() {
                     <span className="loginDiscription">Welcome to BsB Social.Find Your Frends & Post your IDEA!</span>
                 </div>
                 <div className="logingRight">
-                    <div className="loginBox">
-                        <input type="text" className="loginInput" placeholder="UserName" />
-                        <input type="email" className="loginInput" placeholder="Email" />
-                        <input type="password" className="loginInput" placeholder="Password" />
-                        <input type="password" className="loginInput" placeholder="Password Again" />
-                        <button className="loginButoon">Sign Up</button>
+                    <form onSubmit={saveUser} className="loginBox">
+                        <input type="text" className="loginInput" placeholder="UserName" ref={username}/>
+                        <input type="email" className="loginInput" placeholder="Email" ref={email}/>
+                        <input type="password" className="loginInput" placeholder="Password" min="6" ref={password}/>
+                        <input type="password" className="loginInput" placeholder="Password Again" ref={passwordAgain} />
+                        <button className="loginButoon" type="submit">Sign Up</button>
                         <button className="loginregister">Log In to Account</button>
-                    </div>
+                    </form>
                 </div>
             </div>
             
